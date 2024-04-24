ef61f95ea854a0f9df4c8dee9ef1fb9422cf7808
BADRYZ <badryzakaria1@gmail.com>
2024-04-24 15:35:28 +0100

    seance 4

--- app/controllers/utilisateurs_controller.rb
+++ app/controllers/utilisateurs_controller.rb
@@ -2,25 +2,28 @@ class UtilisateursController < ApplicationController
   def new
     @utilisateur = Utilisateur.new
   end
+
   def show
     @utilisateur = Utilisateur.find(params[:id])
   end
+
   def create
-    @utilisateur=Utilisateur.new(utilisateur_params)
+    @utilisateur = Utilisateur.new(utilisateur_params)
     if @utilisateur.save
-      flash[:success] ="Bienvenue!"
-          redirect_to @utilisateur
+      flash[:success] = "Bienvenue!"
+      redirect_to @utilisateur
     else
       render 'new'
     end
   end
 
+  def edit
+    @utilisateur = Utilisateur.find(params[:id])
+  end
+
   private
-    def utilisateur_params
-      params.require(:utilisateur).permit(
-        :nom,:email,:password,:password_confirmation
-      )
-    end
 
-
+  def utilisateur_params
+    params.require(:utilisateur).permit(:nom, :email, :password, :password_confirmation)
+  end
 end
